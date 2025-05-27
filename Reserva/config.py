654c16977f4c1968d74f83a6e0d8f76ff27ea557
Giovana Lopes <gilopes2005@gmail.com>
2025-05-26 21:50:02 -0300

    reservas

--- Reserva/config.py
+++ Reserva/config.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['HOST'] = '0.0.0.0'
-app.config['PORT']=5002
+app.config['PORT']=5000
 app.config['DEBUG'] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- Reserva/reserva_model.py
+++ Reserva/reserva_model.py
@@ -5,10 +5,11 @@ class Reserva(db.Model):
 
     id_reserva = db.Column(db.Integer, primary_key=True)
     turma_id = db.Column(db.Integer, nullable=False)
+    professor = db.Column(db.String(50), nullable=False)
     sala = db.Column(db.String(50), nullable=False)
-    data = db.Column(db.String(20), nullable=False)
-    hora_inicio = db.Column(db.String(10), nullable=False)
-    hora_fim = db.Column(db.String(10), nullable=False)
+    data = db.Column(db.Date, nullable=False)
+    hora_inicio = db.Column(db.Time, nullable=False)
+    hora_fim = db.Column(db.Time, nullable=False)
 
 class ReservaNaoEncontrada(Exception):
     pass
